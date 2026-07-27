0bd627f741d6bbb701f50d8b52a5a2f3bf1dc6cc
Dmytro <nova_dima@ukr.net>
2026-07-27 03:18:13 +0300

    create HomePage

---
+++ components/Header/Header.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Link from 'next/link';
+import css from './Header.module.css';
+
+import { usePathname } from 'next/navigation';
+
+export default function Header() {
+  const pathname = usePathname();
+  const isActive = pathname === '/';
+
+  return (
+    <header className={css.header}>
+      <div className={css.logo}>TravelTrucks</div>
+
+      <nav className={css.nav}>
+        <Link href="/" className={isActive ? `${css.active}` : ''}>
+          Home
+        </Link>
+        <Link href="/catalog" className={!isActive ? `${css.active}` : ''}>
+          Catalog
+        </Link>
+      </nav>
+
+      <div className={css.empty}></div>
+    </header>
+  );
+}
